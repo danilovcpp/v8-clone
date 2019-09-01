ab94efb2bf6ca8437785fc65f7814256b13b2b5e
danilovcpp <danilov.cpp@gmail.com>
2019-09-01 09:27:38 +0600

    Add scanner

---
+++ src/parsing/scanner.h
@@ -0,0 +1,17 @@
+#ifndef V8_PARSING_SCANNER_H_
+#define V8_PARSING_SCANNER_H_
+
+namespace v8
+{
+namespace internal
+{
+class Scanner
+{
+public:
+private:
+};
+
+} // namespace internal
+} // namespace v8
+
+#endif // V8_PARSING_SCANNER_H_
