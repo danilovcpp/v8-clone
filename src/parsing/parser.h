767c85fd38068807cb86881970a5fd073a3f2bc8
danilovcpp <danilov.cpp@gmail.com>
2019-09-01 09:59:13 +0600

    Add parser

---
+++ src/parsing/parser.h
@@ -0,0 +1,17 @@
+#ifndef V8_PARSING_PARSER_H_
+#define V8_PARSING_PARSER_H_
+
+namespace v8
+{
+namespace internal
+{
+class Parser
+{
+public:
+private:
+};
+
+} // namespace internal
+} // namespace v8
+
+#endif // V8_PARSING_PARSER_H_
